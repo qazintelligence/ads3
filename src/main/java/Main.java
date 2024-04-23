9abad75c2841c289e826189f869d5bff331c33e7
- <->
2024-04-23 19:38:49 +0500

    first one

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,34 @@
-public class Main {
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList arr[] = new ArrayList[n];
+        for(int i=0;i<n;i++) {
+            int size = sc.nextInt();
+            arr[i] = new ArrayList();
+            for(int j=0;j<size;j++) {
+                arr[i].add(sc.nextInt());
+            }
+        }
+        int q = sc.nextInt();
+        for(int i=0;i<q;i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            try
+            {
+                System.out.println(arr[x-1].get(y-1));
+            }
+            catch(Exception e)
+            {
+                System.out.println("ERROR!");
+            }
+        }
+        sc.close();
+    }
 }
